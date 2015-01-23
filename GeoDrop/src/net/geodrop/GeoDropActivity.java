effe422d6db1521601e2b94f5d9f36420ff44a49
Nandor Licker <licker.nandor@gmail.com>
2015-01-23 18:46:41 +0000

    Blank Android project.

---
+++ GeoDrop/src/net/geodrop/GeoDropActivity.java
@@ -0,0 +1,15 @@
+package net.geodrop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GeoDropActivity extends Activity {
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
