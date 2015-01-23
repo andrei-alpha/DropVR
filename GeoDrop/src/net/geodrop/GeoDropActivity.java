ec04dd59307253a1b09168b2f4b2dc1adf146b33
Nandor Licker <licker.nandor@gmail.com>
2015-01-23 20:04:44 +0000

    Skeleton code.

--- GeoDrop/src/net/geodrop/GeoDropActivity.java
+++ GeoDrop/src/net/geodrop/GeoDropActivity.java
@@ -2,8 +2,9 @@ package net.geodrop;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.vrtoolkit.cardboard.CardboardActivity;
 
-public class GeoDropActivity extends Activity {
+public class GeoDropActivity extends CardboardActivity {
   /**
    * Called when the activity is first created.
    */
